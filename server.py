b4a443266da3b683fa0f82101c622985014c064c
Jonas Alves <jonasalves@alu.ufc.br>
2022-10-26 15:06:45 -0300

    Add hello endpoint

--- server.py
+++ server.py
@@ -63,6 +63,10 @@ def privacy():
 	# Endpoint for Google Play Store
 	return "Nenhum dado é salvo."
 
+@app.route('/hello', methods=['GET'])
+def hello():
+	return "Hello World!"
+
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT"))
 	app.run(host='0.0.0.0', port=port)
