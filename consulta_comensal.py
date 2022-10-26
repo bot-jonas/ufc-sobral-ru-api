2422883ae917544701be4e9995967a9bbf2080b5
Jonas Alves <jonasalves@alu.ufc.br>
2022-10-26 14:52:35 -0300

    first commit

---
+++ consulta_comensal.py
@@ -0,0 +1,132 @@
+# SSL bug with old sites
+# https://github.com/psf/requests/issues/4775#issuecomment-478198879
+
+import requests
+import ssl
+from urllib3 import poolmanager
+import re
+
+class TLSAdapter(requests.adapters.HTTPAdapter):
+    def init_poolmanager(self, connections, maxsize, block=False):
+        """Create and initialize the urllib3 PoolManager."""
+        ctx = ssl.create_default_context()
+        ctx.set_ciphers('DEFAULT@SECLEVEL=1')
+        self.poolmanager = poolmanager.PoolManager(
+                num_pools=connections,
+                maxsize=maxsize,
+                block=block,
+                ssl_version=ssl.PROTOCOL_TLS,
+                ssl_context=ctx)
+
+s = requests.session()
+s.mount('https://', TLSAdapter())
+
+def get_javax_faces_ViewState(html):
+	return re.findall('<input type="hidden" name="javax.faces.ViewState" id="javax.faces.ViewState" value="(.*?)" />', html)[0]
+
+def login(card_number, registration):
+	# Clear old cookies
+	s.cookies.clear()
+
+	# Get cookies/params
+	r = s.get("https://si3.ufc.br/public/jsp/restaurante_universitario/consulta_comensal_ru.jsf")
+	
+	jfv = get_javax_faces_ViewState(r.text)
+	JSESSIONID = r.cookies.get("JSESSIONID")
+
+	# Login
+	body = {
+		"form": "form",
+		"form:j_id_jsp_1091681061_2": card_number,
+		"form:j_id_jsp_1091681061_3": registration,
+		"form:j_id_jsp_1091681061_4": "Consultar",
+		"javax.faces.ViewState": jfv,
+	}
+
+	r = s.post(f"https://si3.ufc.br/public/jsp/restaurante_universitario/consulta_comensal_ru.jsf;jsessionid={JSESSIONID}", data=body)
+
+	jfv = get_javax_faces_ViewState(r.text)
+
+	return jfv, JSESSIONID
+
+def generate_gru(amount_credits, card_number, registration):
+	# Generate pre-request
+	jfv, JSESSIONID = login(card_number, registration)
+
+	body = {
+		# "AJAXREQUEST": "j_id_jsp_540432864_0",
+		"form": "form",
+		"form:qtdCreditos": amount_credits,
+		"javax.faces.ViewState": jfv,
+		# "form:j_id_jsp_540432864_3": "form:j_id_jsp_540432864_3",
+	}
+
+	r = s.post(f"https://si3.ufc.br/public/jsp/restaurante_universitario/gera_gru_restaurante.jsf", data=body)
+
+	# Get generated GRU
+	body = {
+		"modalForm": "modalForm",
+		"modalForm:btConfirmarGeracao": "Estou ciente",
+		"javax.faces.ViewState": jfv,
+	}
+
+	r = s.post(f"https://si3.ufc.br/public/jsp/restaurante_universitario/gera_gru_restaurante.jsf", data=body)
+
+	return r.content
+
+def generate_pix(amount_credits, card_number, registration):
+	# Generate pre-request
+	jfv, JSESSIONID = login(card_number, registration)
+
+	body = {
+		# "AJAXREQUEST": "j_id_jsp_540432864_0",
+		"form": "form",
+		"form:qtdCreditos": amount_credits,
+		"javax.faces.ViewState": jfv,
+		# "form:j_id_jsp_540432864_3": "form:j_id_jsp_540432864_3",
+	}
+
+	r = s.post(f"https://si3.ufc.br/public/jsp/restaurante_universitario/gera_gru_restaurante.jsf", data=body)
+
+	jfv = get_javax_faces_ViewState(r.text)
+
+	# Get idSessao from generated PagTesouro page
+	body = {
+		"modalForm2": "modalForm2",
+		"modalForm2:btConfirmarPagtesouro": "Estou ciente",
+		"javax.faces.ViewState": jfv,
+	}
+
+	r = s.post(f"https://si3.ufc.br/public/jsp/restaurante_universitario/gera_gru_restaurante.jsf", data=body)
+
+	idSessao = re.findall(r'<iframe class="iframe-epag"(?:(?:\n|\t|.)*?) src="https://pagtesouro.tesouro.gov.br/#/pagamento\?idSessao=(.*?)&', r.text)[0]
+
+	# Send idSessao to dados-pagamento
+	r = s.get(f"https://pagtesouro.tesouro.gov.br/api/pagamentos/dados-pagamento?idSessao={idSessao}")
+	payment_details = r.json()
+
+	# Get PIX info
+	body = {
+		"idSessao": idSessao,
+		"informacoesAdicionais": [
+			{
+				"nome": "Origem",
+				"valor": "PagTesouro"
+			},
+			{
+				"nome": "Serviço",
+				"valor": "6605 - Pagamento de taxa por utilização do RU"
+			}
+		]
+	}
+
+	r = s.post("https://pagtesouro.tesouro.gov.br/api/pagamentos/meios-pagamento/pix", json=body)
+	pix_details = r.json()
+
+	response = {
+		"payment_details": payment_details,
+		"pix_details": pix_details,
+		"id_sessao": idSessao,
+	}
+
+	return response
---
+++ server.py
@@ -0,0 +1,68 @@
+import os
+import json
+from flask import Flask, request, make_response
+import traceback as tb
+
+from consulta_comensal import generate_gru, generate_pix
+
+app = Flask(__name__)
+
+@app.route('/gru.pdf', methods=['GET'])
+def gru():
+	amount_credits = request.args.get('amount_credits')
+	card_number = request.args.get('card_number')
+	registration = request.args.get('registration')
+
+	data = generate_gru(amount_credits, card_number, registration)
+
+	response = make_response(data)
+
+	response.headers.set('Content-Type', 'application/pdf')
+	# response.headers.set('Content-Disposition', 'attachment', filename='gru.pdf')
+	response.headers.set("Cache-Control", "no-cache, no-store, must-revalidate")
+	response.headers.set("Pragma", "no-cache")
+	response.headers.set("Expires", "0")
+	response.headers.set("Cache-Control", "public, max-age=0")
+
+	return response
+
+@app.route('/pix', methods=['POST'])
+def pix():
+	inp = request.get_json(force=True)
+
+	try:
+		amount_credits = inp["amount_credits"]
+		card_number = inp["card_number"]
+		registration = inp["registration"]
+	except:
+		return {
+			"ok": False,
+			"errors": [
+				"Todos os parâmetros (amount_credits, card_number, registration) são necessários!",
+			],
+		}, 400
+
+	try:
+		data = generate_pix(amount_credits, card_number, registration)
+	except:
+		print(tb.format_exc())
+		return {
+			"ok": False,
+			"errors": [
+				"Não foi possível gerar o PIX!",
+			],
+		}, 500
+
+	response = make_response({"ok": True, "data": data,})
+	response.headers.set('Content-Type', 'application/json')
+
+	return response
+
+@app.route('/privacy', methods=['GET'])
+def privacy():
+	# Endpoint for Google Play Store
+	return "Nenhum dado é salvo."
+
+if __name__ == "__main__":
+	port = int(os.environ.get("PORT"))
+	app.run(host='0.0.0.0', port=port)
